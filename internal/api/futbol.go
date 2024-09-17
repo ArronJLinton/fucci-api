5a43ddde7da04f6f90928752c30472a4ee385508
Arron Linton <arronlinton@Arrons-MBP.fios-router.home>
2024-09-17 18:58:41 -0400

    re-org type declartions

--- internal/api/futbol.go
+++ internal/api/futbol.go
@@ -4,92 +4,10 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 )
 
 type GetMatchesParams struct {
 	Date string
-}
-
-type GetMatchesAPIResponse struct {
-	Get        string `json:"get"`
-	Parameters struct {
-		Date string `json:"date"`
-	} `json:"parameters"`
-	Errors  []any `json:"errors"`
-	Results int   `json:"results"`
-	Paging  struct {
-		Current int `json:"current"`
-		Total   int `json:"total"`
-	} `json:"paging"`
-	Response []struct {
-		Fixture struct {
-			ID        int       `json:"id"`
-			Referee   string    `json:"referee"`
-			Timezone  string    `json:"timezone"`
-			Date      time.Time `json:"date"`
-			Timestamp int       `json:"timestamp"`
-			Periods   struct {
-				First  int `json:"first"`
-				Second int `json:"second"`
-			} `json:"periods"`
-			Venue struct {
-				ID   int    `json:"id"`
-				Name string `json:"name"`
-				City string `json:"city"`
-			} `json:"venue"`
-			Status struct {
-				Long    string `json:"long"`
-				Short   string `json:"short"`
-				Elapsed int    `json:"elapsed"`
-			} `json:"status"`
-		} `json:"fixture"`
-		League struct {
-			ID      int    `json:"id"`
-			Name    string `json:"name"`
-			Country string `json:"country"`
-			Logo    string `json:"logo"`
-			Flag    any    `json:"flag"`
-			Season  int    `json:"season"`
-			Round   string `json:"round"`
-		} `json:"league"`
-		Teams struct {
-			Home struct {
-				ID     int    `json:"id"`
-				Name   string `json:"name"`
-				Logo   string `json:"logo"`
-				Winner any    `json:"winner"`
-			} `json:"home"`
-			Away struct {
-				ID     int    `json:"id"`
-				Name   string `json:"name"`
-				Logo   string `json:"logo"`
-				Winner any    `json:"winner"`
-			} `json:"away"`
-		} `json:"teams"`
-		Goals struct {
-			Home int `json:"home"`
-			Away int `json:"away"`
-		} `json:"goals"`
-		Score struct {
-			Halftime struct {
-				Home int `json:"home"`
-				Away int `json:"away"`
-			} `json:"halftime"`
-			Fulltime struct {
-				Home int `json:"home"`
-				Away int `json:"away"`
-			} `json:"fulltime"`
-			Extratime struct {
-				Home any `json:"home"`
-				Away any `json:"away"`
-			} `json:"extratime"`
-			Penalty struct {
-				Home any `json:"home"`
-				Away any `json:"away"`
-			} `json:"penalty"`
-		} `json:"score"`
-	} `json:"response"`
 }
 
 func (c *Config) getMatches(w http.ResponseWriter, r *http.Request) {
@@ -250,76 +168,6 @@ func (c *Config) getTeamSquad(id int32) (*GetSquadResponse, error) {
 		return nil, fmt.Errorf("error creating http request: %s", err)
 	}
 	return response, nil
-}
-
-type GetLineUpResponse struct {
-	Get        string `json:"get"`
-	Parameters struct {
-		Fixture string `json:"fixture"`
-	} `json:"parameters"`
-	Errors  []any `json:"errors"`
-	Results int   `json:"results"`
-	Paging  struct {
-		Current int `json:"current"`
-		Total   int `json:"total"`
-	} `json:"paging"`
-	Response []struct {
-		Team struct {
-			ID     int    `json:"id"`
-			Name   string `json:"name"`
-			Logo   string `json:"logo"`
-			Colors any    `json:"colors"`
-		} `json:"team"`
-		Coach struct {
-			ID    int    `json:"id"`
-			Name  string `json:"name"`
-			Photo string `json:"photo"`
-		} `json:"coach"`
-		Formation string `json:"formation"`
-		StartXI   []struct {
-			Player Player `json:"player"`
-		} `json:"startXI"`
-		Substitutes []struct {
-			Player struct {
-				ID     int    `json:"id"`
-				Name   string `json:"name"`
-				Number int    `json:"number"`
-				Pos    string `json:"pos"`
-				Grid   any    `json:"grid"`
-				Photo  string `json:"photo"`
-			} `json:"player"`
-		} `json:"substitutes"`
-	} `json:"response"`
-}
-
-type GetSquadResponse struct {
-	Get        string `json:"get"`
-	Parameters struct {
-		Team string `json:"team"`
-	} `json:"parameters"`
-	Errors  []any `json:"errors"`
-	Results int   `json:"results"`
-	Paging  struct {
-		Current int `json:"current"`
-		Total   int `json:"total"`
-	} `json:"paging"`
-	Response []struct {
-		Team struct {
-			ID   int    `json:"id"`
-			Name string `json:"name"`
-			Logo string `json:"logo"`
-		} `json:"team"`
-		Players []Player `json:"players"`
-	} `json:"response"`
-}
-
-type Player struct {
-	ID     int    `json:"id"`
-	Name   string `json:"name"`
-	Number int    `json:"number"`
-	Pos    string `json:"pos"`
-	Grid   string `json:"grid"`
-	Photo  string `json:"photo"`
 }
 
 func filterByName(items []Player, name string) Player {
